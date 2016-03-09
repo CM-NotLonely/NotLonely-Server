eed6659d3484f972b1b0a91994a373cd517a0fbc
dukuikui <k3266904708k@gmail.com>
2016-03-09 21:56:45 +0800

    完成了group头像上传的问题。

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,5 @@ class Group < ActiveRecord::Base
 	belongs_to :user
 	has_many :activities
 	has_many :group_applies
+	mount_uploader :avatar, AvatarUploader
 end
---
+++ db/migrate/20160309135422_delete_headimg_to_groups.rb
@@ -0,0 +1,4 @@
+class DeleteHeadimgToGroups < ActiveRecord::Migration
+  def change
+  end
+end
