c98e8c18fb87da129d18ab7f9ad3e88a2f926027
dukuikui <k3266904708k@gmail.com>
2016-03-11 12:46:21 +0800

    添加了白名单的允许参数：avatar

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,20 +1,20 @@
 class GroupsController < ApplicationController
-	def new
-
-	end
-
+	before_action :set_user
 	def create
-		@group=Group.new(group_params)
+		@group = @user.groups.new(group_params)
 		if @group.save
-
+			render json: {code: 0, group: @group}
 		else
-
+			render json: {code: 3001}
 		end
 	end
 
 	private
 	def group_params
-		params.require(:group).permit(:title, :introduction, :heading)
+		params.permit(:title, :introduction, :avatar)
 	end
 
+	def set_user
+		@user = User.find_by(id: session[:user_id])
+	end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,7 @@ class UserController < ApplicationController
 	#注册账户的动作跳过过滤器。
 	skip_before_action :identify, only: [:create]
 	#设置前置过滤器，首先确定user。
-	before_action :set_user, only: [:show]
+	before_action :set_user, only: [:show, :update]
 	def create
 		@user = User.new(params_user)
 		@user.avatar = params[:file]
@@ -29,7 +29,6 @@ class UserController < ApplicationController
 	end
 
 	def update
-		@user = User.where(id: session[:user_id]).take
 		@user.avatar = params[:file]
 		if @user.update(params_user_need)
 			render json: {code: 0, user: @user}
@@ -44,11 +43,11 @@ class UserController < ApplicationController
 		end
 
 		def params_user_need
-			params.permit(:password, :nickname, :sex, :introduction)
+			params.permit(:password, :nickname, :sex, :introduction, :avatar)
 		end
 
 		def set_user
-			@user = User.find(session[:user_id])
+			@user = User.find_by(id: session[:user_id])
 		end
 end
 
