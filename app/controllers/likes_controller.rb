2f52eaf20153ce231720e88b3946915f4ff2686d
msl12 <2496227361@qq.com>
2016-03-14 13:34:30 +0800

    解决了重复点赞的BUG

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,13 @@
 class LikesController < ApplicationController
-	#点赞
+	#点赞，我觉得用数据库来检验是否重复点赞比较好些
 	def create
 		activity = Activity.find(params[:activity_id])
 		like = activity.likes.new
 		like.user_id = session[:user_id]
-		if like.save
+		user=User.find_by(id: session[:user_id])
+		if user.likes.find_by(activity_id: activity.id)
+			render json: {code: 3001, message: '不能重复点赞'}
+		elsif like.save
 			render json: {code: 0, like: like}
 		else
 			render json: {code: 3001}
